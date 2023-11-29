a6fc85e69ea5be62d8d378c4834ad444d92cb033
christianniehaus <128558829+christianniehaus@users.noreply.github.com>
2023-11-29 13:07:32 -0500

    remove old commented out code

--- src/main/java/me/barny1094875/utilitiesog/modules/ColorCodes.java
+++ src/main/java/me/barny1094875/utilitiesog/modules/ColorCodes.java
@@ -31,18 +31,6 @@ public class ColorCodes implements CommandExecutor {
                 Player player = (Player) sender;
 
                 // Send the color code cheat sheet to the player.
-//                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b&l-=-=-=-=- &5&lC&3&lo&a&ll&e&lo&c&lr &B&lC&d&lo&f&ld&d&le&B&ls: &b&l-=-=-=-=-"));
-//                player.sendMessage("&0 " + ChatColor.translateAlternateColorCodes('&', "&f= &0Black               &r") + "&1 " + ChatColor.translateAlternateColorCodes('&', "&f= &1Dark Blue"));
-//                player.sendMessage("&2 " + ChatColor.translateAlternateColorCodes('&', "&f= &2Dark Green       &r") + "&3 " + ChatColor.translateAlternateColorCodes('&', "&f= &3Dark Aqua"));
-//                player.sendMessage("&4 " + ChatColor.translateAlternateColorCodes('&', "&f= &4Dark Red          &r") + "&5 " + ChatColor.translateAlternateColorCodes('&', "&f= &5Dark Purple"));
-//                player.sendMessage("&6 " + ChatColor.translateAlternateColorCodes('&', "&f= &6Gold                &r") + "&7 " + ChatColor.translateAlternateColorCodes('&', "&f= &7Grey"));
-//                player.sendMessage("&8 " + ChatColor.translateAlternateColorCodes('&', "&f= &8Dark Grey         &r") + "&9 " + ChatColor.translateAlternateColorCodes('&', "&f= &9Blue"));
-//                player.sendMessage("&a " + ChatColor.translateAlternateColorCodes('&', "&f= &aGreen              &r") + "&b " + ChatColor.translateAlternateColorCodes('&', "&f= &bAqua"));
-//                player.sendMessage("&c " + ChatColor.translateAlternateColorCodes('&', "&f= &cRed                 &r") + "&d " + ChatColor.translateAlternateColorCodes('&', "&f= &dLight Purple"));
-//                player.sendMessage("&e " + ChatColor.translateAlternateColorCodes('&', "&f= &eYellow              &r") + "&f " + ChatColor.translateAlternateColorCodes('&', "&f= &fWhite"));
-//                player.sendMessage("&k " + ChatColor.translateAlternateColorCodes('&', "&f= &kMagic               &r") + "&l " + ChatColor.translateAlternateColorCodes('&', " &f= &lBold"));
-//                player.sendMessage("&m " + ChatColor.translateAlternateColorCodes('&', "&f= &mStrikethrough&r    ") + "&n " + ChatColor.translateAlternateColorCodes('&', "&f= &nUnderline"));
-//                player.sendMessage("&o " + ChatColor.translateAlternateColorCodes('&', "&f= &oItalic               &r") + "&r " + ChatColor.translateAlternateColorCodes('&', "&f= &rReset"));
                 player.sendMessage(Utilities_OG.getMM().deserialize("<dark_aqua><bold>-=-=-=-=- <dark_purple>C<dark_aqua>o<green>l<yellow>o<red>r <aqua>C<light_purple>o<white>d<light_purple>e<aqua>s: -=-=-=-=-"));
                 player.sendMessage(Utilities_OG.getMM().deserialize("&0 = <black>Black               <reset> &1 = <dark_blue>Dark Blue"));
                 player.sendMessage(Utilities_OG.getMM().deserialize("&2 = <dark_green>Dark Green       <reset> &3 = <dark_aqua>Dark Aqua"));
