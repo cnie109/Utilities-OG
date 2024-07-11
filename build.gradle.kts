01e1d4b95b4bea4a201a124567d19ccd2d2ccc82
NotAlexNoyle <alexnoyle@icloud.com>
2024-07-11 08:39:31 -0400

    Update gradle

---
+++ build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    java // Tell gradle this is a java project.
+    id("io.github.goooler.shadow") version "8.1.8"
+    eclipse // Import eclipse plugin for IDE integration.
+    kotlin("jvm") version "1.9.23" // Import kotlin jvm plugin for kotlin/java integration.
+}
+
+java {
+    // Declare java version.
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+group = "me.barny1094875" // Declare bundle identifier.
+version = "1.3.3" // Declare plugin version (will be in .jar).
+val apiVersion = "1.19" // Declare minecraft server target version.
+
+tasks.named<ProcessResources>("processResources") {
+    val props = mapOf(
+        "version" to version,
+        "apiVersion" to apiVersion
+    )
+
+    inputs.properties(props) // Indicates to rerun if version changes.
+
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+repositories {
+    mavenCentral()
+
+    maven {
+        url = uri("https://repo.purpurmc.org/snapshots")
+    }
+    
+    maven {
+    
+    	url = uri("https://repo.codemc.io/repository/maven-public/")
+    
+    }
+    
+    maven {
+    
+    	url = uri("https://maven.enginehub.org/repo/")
+    
+    }
+}
+
+dependencies {
+    compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT") // Declare purpur API version to be packaged.
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.8") // Import WorldGuard API.
+    compileOnly("de.tr7zw:item-nbt-api-plugin:2.11.2") // Import NBT API.
+    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3") // Import MiniPlaceholders API.
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
+
+tasks.shadowJar {
+    exclude("io.github.miniplaceholders.*") // Exclude the MiniPlaceholders package from being shadowed.
+    minimize()
+}
+
+tasks.jar {
+    dependsOn(tasks.shadowJar)
+    archiveClassifier.set("part")
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("") // Use empty string instead of null
+    from("LICENSE") {
+        into("/")
+    }
+}
+
+tasks.jar {
+    dependsOn("shadowJar")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+    options.encoding = "UTF-8"
+    options.isFork = true
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+        vendor = JvmVendorSpec.GRAAL_VM
+    }
+}
